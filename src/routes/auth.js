786c2effe28310342e03c2193230292c8a0d2c0a
patrice <patricedev02@gmail.com>
2023-12-17 21:38:01 +0000

    refresh token

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+const { register, login, logout } = require("../controllers/auth");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 
 authRouter.post("/login", login);
+
+authRouter.get("/:id/logout", logout);
 
 module.exports = authRouter;
